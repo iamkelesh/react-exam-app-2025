88ee960e7545b0f89613739a6077c5b32916c569
Deyan Kamburov <71717701+iamkelesh@users.noreply.github.com>
2024-08-14 17:17:28 +0300

    createt small post component

--- client-app/src/components/smallPostTemplate/SmallPostTemplate.jsx
+++ client-app/src/components/smallPostTemplate/SmallPostTemplate.jsx
@@ -1,28 +1,25 @@
-function SmallPostTemplate() {
+function SmallPostTemplate({_id, title, body}) {
     return (
-        <div className="item mb-5">
+        <div className="item mb-5" id={_id}>
             <div className="media">
-                <img
+                {/* <img
                     className="mr-3 img-fluid post-thumb d-none d-md-flex"
                     src="assets/images/blog/blog-post-thumb-4.jpg"
                     alt="image"
-                />
+                /> */}
                 <div className="media-body">
                     <h3 className="title mb-1">
-                        <a href="blog-post.html">Top 5 JavaScript Frameworks</a>
+                        <a href="blog-post.html">{title}</a>
                     </h3>
                     <div className="meta mb-1">
-                        <span className="date">Published 2 months ago</span>
-                        <span className="time">15 min read</span>
-                        <span className="comment">
+                        {/* <span className="date">Published 2 months ago</span> */}
+                        {/* <span className="time">15 min read</span> */}
+                        {/* <span className="comment">
                             <a href="#">3 comments</a>
-                        </span>
+                        </span> */}
                     </div>
                     <div className="intro">
-                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-                        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-                        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-                        ultricies...
+                        {(body.length > 100) ? body.substring(0, 200) + '...' : body}
                     </div>
                     <a className="more-link" href="blog-post.html">
                         Read more →
