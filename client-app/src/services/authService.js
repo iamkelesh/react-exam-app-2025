fa077702724fb492192ae31787e5216208ff91a6
Deyan Kamburov <dbkamburov00@gmail.com>
2025-01-05 00:55:36 +0200

    removed repeatPassword from sending to server

--- client-app/src/services/authService.js
+++ client-app/src/services/authService.js
@@ -4,11 +4,11 @@ const baseUrl = 'http://localhost:3030/users';
 
 
 export const register = async ({ values }) => {
-    return await requester.post({ url: `${baseUrl}/register`, values })
+    return await requester.post({ url: `${baseUrl}/register`, values: {fullName:values.fullName, email: values.email, password: values.password } })
 }
 
 export const login = async ({ values }) => {
-    return await requester.post({url: `${baseUrl}/login`, values})
+    return await requester.post({ url: `${baseUrl}/login`, values })
 }
 
 export const logout = () => requester.get(`${baseUrl}/logout`)
