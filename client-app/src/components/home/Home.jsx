78b06825c417ffa7c768643e0d6d1f0b8a564ce4
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-19 23:11:08 +0200

    refatored home for tailwind

--- client-app/src/components/home/Home.jsx
+++ client-app/src/components/home/Home.jsx
@@ -49,45 +49,61 @@ function Home() {
     }, [pageNumber, currentPage])
 
     return (
-        <div className="main-wrapper">
-            <section className="cta-section theme-bg-light py-5">
-                <div className="container text-center">
-                    <h2 className="heading">DevBlog - A Blog Template Made For Developers</h2>
-                </div>
-            </section>
-            <section className="blog-list px-3 py-5 p-md-5">
-                <div className="container">
+        <section class="text-gray-600 body-font overflow-hidden">
+            <div class="container px-5 py-24 mx-auto">
+                <div class="-my-8 divide-y-2 divide-gray-100">
+
 
                     {posts.map(data => <SmallPostTemplate key={data.id} {...data} />)}
                     {posts.length === 0 ? <h1>There are no posts!</h1> : ''}
 
 
-                    <nav className="blog-nav nav nav-justified my-5">
-
-                        {currentPage > 0 ? <Link
-                            className={"nav-link-prev nav-item nav-link  rounded-left"}
-
-                            to={`/home/${currentPage - 1}`}
-                        >
-                            Previous
-                            <i className="arrow-prev fas fa-long-arrow-alt-left" />
-                        </Link> : ''}
-                        {moreAvailable === true ?
-                            <Link
-                                className="nav-link-next nav-item nav-link rounded"
-                                to={`/home/${currentPage + 1}`}
-                            >
-                                Next
-                                <i className="arrow-next fas fa-long-arrow-alt-right" />
-                            </Link> : ""}
-                    </nav>
-
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
 
     );
 }
 
 
 export default Home;
+
+
+
+
+// <div className="main-wrapper">
+//     <section className="cta-section theme-bg-light py-5">
+//         <div className="container text-center">
+//             <h2 className="heading">DevBlog - A Blog Template Made For Developers</h2>
+//         </div>
+//     </section>
+//     <section className="blog-list px-3 py-5 p-md-5">
+//         <div className="container">
+
+//             {posts.map(data => <SmallPostTemplate key={data.id} {...data} />)}
+//             {posts.length === 0 ? <h1>There are no posts!</h1> : ''}
+
+
+//             <nav className="blog-nav nav nav-justified my-5">
+
+//                 {currentPage > 0 ? <Link
+//                     className={"nav-link-prev nav-item nav-link  rounded-left"}
+
+//                     to={`/home/${currentPage - 1}`}
+//                 >
+//                     Previous
+//                     <i className="arrow-prev fas fa-long-arrow-alt-left" />
+//                 </Link> : ''}
+//                 {moreAvailable === true ?
+//                     <Link
+//                         className="nav-link-next nav-item nav-link rounded"
+//                         to={`/home/${currentPage + 1}`}
+//                     >
+//                         Next
+//                         <i className="arrow-next fas fa-long-arrow-alt-right" />
+//                     </Link> : ""}
+//             </nav>
+
+//         </div>
+//     </section>
+// </div>
