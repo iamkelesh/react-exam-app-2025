52a87408652ce4a3c72bd3bda3b43545dd995328
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-19 23:59:57 +0200

    updated Favourites html

--- client-app/src/components/favourites/Favourites.jsx
+++ client-app/src/components/favourites/Favourites.jsx
@@ -46,41 +46,25 @@ function Favourites() {
     }, [pageNumber, currentPage])
 
     return (
-        <div className="main-wrapper">
-            <section className="cta-section theme-bg-light py-5">
-                <div className="container text-center">
-                    <h2 className="heading">DevBlog - A Blog Template Made For Developers</h2>
+        <section class="text-gray-600 body-font overflow-hidden">
+            <div class="container px-5 py-24 mx-auto">
+                <div class="-my-8 divide-y-2 divide-gray-100">
 
-                </div>
-            </section>
-            <section className="blog-list px-3 py-5 p-md-5">
-                <div className="container">
+
                     {favPosts.map(data => <SmallPostTemplate key={data.id} {...data} />)}
                     {favPosts.length === 0 ? <h1>There are no posts!</h1> : ''}
 
-                    <nav className="blog-nav nav nav-justified my-5">
 
-                        {currentPage > 0 ? <Link
-                            className={"nav-link-prev nav-item nav-link  rounded-left"}
-
-                            to={`/user/posts/${currentPage - 1}`}
-                        >
-                            Previous
-                            <i className="arrow-prev fas fa-long-arrow-alt-left" />
-                        </Link> : ''}
-                        {moreAvailable === true ?
-                            <Link
-                                className="nav-link-next nav-item nav-link rounded"
-                                to={`/user/posts/${currentPage + 1}`}
-                            >
-                                Next
-                                <i className="arrow-next fas fa-long-arrow-alt-right" />
-                            </Link> : ""}
-                    </nav>
+                    {moreAvailable === true || currentPage > 0 ?
+                        <Pagination
+                            defaultUrl={defaultUrl}
+                            currentPage={currentPage}
+                            moreAvailable={moreAvailable}
+                        /> : ""}
 
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
 
     );
 }
