9c3f320987e121de5c03ad18179a3db6b07fcf2f
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-20 00:00:09 +0200

    updated users posts template

--- client-app/src/components/usersPosts/UsersPosts.jsx
+++ client-app/src/components/usersPosts/UsersPosts.jsx
@@ -87,42 +87,26 @@ function Home() {
 
 
     return (
-        <div className="main-wrapper">
-            <section className="cta-section theme-bg-light py-5">
-                <div className="container text-center">
-                    <h2 className="heading">DevBlog - A Blog Template Made For Developers</h2>
+        <section class="text-gray-600 body-font overflow-hidden">
+            <div class="container px-5 py-24 mx-auto">
+                <div class="-my-8 divide-y-2 divide-gray-100">
 
-                </div>
-            </section>
-            <section className="blog-list px-3 py-5 p-md-5">
-                <div className="container">
                     {myPosts.map(data => <SmallPostTemplate key={data.id} {...data} />)}
                     {myPosts.length === 0 ? <h1>There are no posts!</h1> : ''}
 
                     <nav className="blog-nav nav nav-justified my-5">
 
-                        {currentPage > 0 ? <Link
-                            className={"nav-link-prev nav-item nav-link  rounded-left"}
-                            // className={`nav-link-prev nav-item nav-link` + (currentPage > 0 ? ' ' : 'd-none') + `rounded-left`}
-
-                            to={`/user/posts/${currentPage - 1}`}
-                        >
-                            Previous
-                            <i className="arrow-prev fas fa-long-arrow-alt-left" />
-                        </Link> : ''}
-                        {moreAvailable === true ?
-                            <Link
-                                className="nav-link-next nav-item nav-link rounded"
-                                to={`/user/posts/${currentPage + 1}`}
-                            >
-                                Next
-                                <i className="arrow-next fas fa-long-arrow-alt-right" />
-                            </Link> : ""}
+                        {moreAvailable === true || currentPage > 0 ?
+                            <Pagination
+                                defaultUrl={defaultUrl}
+                                currentPage={currentPage}
+                                moreAvailable={moreAvailable}
+                            /> : ""}
                     </nav>
 
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
 
     );
 }
