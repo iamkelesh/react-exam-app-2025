27e9484a2a23d3f142e0df10682a99fd819e49b9
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-23 18:12:42 +0200

    updated template for post details

--- client-app/src/components/postDetails/PostDetails.jsx
+++ client-app/src/components/postDetails/PostDetails.jsx
@@ -51,13 +51,69 @@ function PostDetails() {
     }, [postId, navigate])
 
     return (
-        <article className="blog-post px-3 py-5 p-md-5">
-            <div className="container">
+        <div className="w-full md:w-2/5 mx-auto">
+            <div className="mx-5 my-3 text-sm">
+                <a href="" className=" text-red-600 font-bold tracking-widest">TYPE OF POST {dataState.typeOfPost}</a>
+            </div>
+            <div className="w-full text-gray-800 text-4xl px-5 font-bold leading-none">
+                {dataState.title}
+            </div>
 
-                <header className="blog-post-header">
-                    <h2 className="title mb-2">{dataState.title}</h2>
-                    <div className="meta mb-3">
-                        <span className="date">Published on {formatedDate(dataState.createdAt)}</span>
+            <div className="w-full text-gray-500 px-5 pb-5 pt-2">
+                Sub Title {dataState.subTitle}
+
+            </div>
+
+            {/* <div className="mx-5">
+                <img
+                    src="https://static.politico.com/dims4/default/fcd6d6a/2147483647/resize/1920x/quality/90/?url=https%3A%2F%2Fstatic.politico.com%2F22%2F87%2F2259ffd444678054896b9fa32b4d%2Fgettyimages-1221513169.jpg"
+                    alt="Georgia Gov. Brian Kemp speaks to the media during a press conference. | Kevin C. Cox/Getty Images" />
+            </div> */}
+
+            {/* <div className="w-full text-gray-600 text-normal mx-5">
+                <p className="border-b py-3">Georgia Gov. Brian Kemp speaks to the media during a press conference. | Kevin C.
+                    Cox/Getty Images</p>
+            </div> */}
+
+            <div className="w-full text-gray-600 font-thin italic px-5 pt-3">
+                By <strong className="text-gray-700">Posted by user name {dataState.publisherName}</strong><br />
+                {formatedDate(dataState.createdAt)}<br />
+                {/* Updated: 07/17/2020 10:33 AM EDT */}
+            </div>
+
+            <div className="px-5 w-full mx-auto">
+                <p className="my-5">{dataState.body}</p>
+
+            </div>
+
+            <p>
+                TODO: Add comments
+            </p>
+
+
+            <p>
+                TODO: Add proper buttons for edit and delete
+            </p>
+        </div>
+
+    )
+}
+
+export default PostDetails
+
+
+
+
+
+function oldCode() {
+    return (
+        <article classNameName="blog-post px-3 py-5 p-md-5">
+            <div classNameName="container">
+
+                <header classNameName="blog-post-header">
+                    <h2 classNameName="title mb-2">{dataState.title}</h2>
+                    <div classNameName="meta mb-3">
+                        <span classNameName="date">Published on {formatedDate(dataState.createdAt)}</span>
 
                         {dataState.ownerId === userId && (
                             <>
@@ -69,8 +125,8 @@ function PostDetails() {
                     </div>
                 </header>
 
-                <div className="blog-post-body">
-                    <figure className="blog-banner">
+                <div classNameName="blog-post-body">
+                    <figure classNameName="blog-banner">
 
                     </figure>
                     <p>
@@ -86,14 +142,11 @@ function PostDetails() {
                     body={dataState.body}
                 />}
 
-                <div className="blog-comments-section">
+                <div classNameName="blog-comments-section">
                     <Comments currentUser={userId} />
                 </div>
 
             </div>
         </article>
-
     )
 }
-
-export default PostDetails
