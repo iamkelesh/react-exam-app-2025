d2a3226d8c67328fbb5a7be1bc4495de50fda973
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-28 00:18:27 +0200

    added service for getting post by category

--- client-app/src/services/getPostService.js
+++ client-app/src/services/getPostService.js
@@ -1,6 +1,6 @@
 import { collection, addDoc, getDocs, serverTimestamp, doc, getDoc, orderBy, query, updateDoc, deleteDoc, limit, where, startAfter } from 'firebase/firestore';
 import { firestoreDB } from '../firebase/config';
-import { homePageQuery, mainPagePostsQuery, myPostsQuery,allPostsQuery } from './queries';
+import { homePageQuery, mainPagePostsQuery, myPostsQuery, allPostsQuery, postsByCategoryQuery } from './queries';
 
 const collectionRef = collection(firestoreDB, 'user-posts-test1');
 
@@ -36,18 +36,50 @@ export const getAllPosts = async ({ lastSnapshot }) => {
 
     } catch (error) {
         console.log(error);
-        return { posts: [], lastDoc: null, moreAvailable: false }
+        return { newPosts: [], lastDoc: null, moreAvailable: false }
     }
 
 }
 
+export const getByCategory = async ({ lastSnapshot, category }) => {
+
+    let neededQuery = postsByCategoryQuery({ lastSnapshot, category })
+
+    try {
+        const querySnapshot = await getDocs(neededQuery)
+
+        let moreAvailable = false
+
+        let slicedDocs = querySnapshot.docs
+
+        if (slicedDocs.length > 10) {
+
+            moreAvailable = true
+
+            slicedDocs = slicedDocs.slice(0, 10)
+        }
+
+        let lastDoc = slicedDocs[slicedDocs.length - 1]
+
+        let newPosts = slicedDocs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }))
+
+        return { newPosts, lastDoc, moreAvailable }
+
+    } catch (error) {
+        console.log(error);
+        return { newPosts: [], lastDoc: null, moreAvailable: false }
+    }
+}
 
 export const getLatestHomePost = async () => {
     try {
         const querySnapshot = await getDocs(homePageQuery)
 
         let latestPostResult = querySnapshot.docs.map(doc => {
-            
+
             const docData = doc.data()
 
             const docId = doc.id
