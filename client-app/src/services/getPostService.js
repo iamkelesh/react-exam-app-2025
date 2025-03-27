879092e0fea5a95109fb27d364b527dc5e87ede1
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-27 21:41:31 +0200

    refactored getAllPost service

--- client-app/src/services/getPostService.js
+++ client-app/src/services/getPostService.js
@@ -1,74 +1,38 @@
 import { collection, addDoc, getDocs, serverTimestamp, doc, getDoc, orderBy, query, updateDoc, deleteDoc, limit, where, startAfter } from 'firebase/firestore';
 import { firestoreDB } from '../firebase/config';
-import { homePageQuery, mainPagePostsQuery, myPostsQuery } from './queries';
+import { homePageQuery, mainPagePostsQuery, myPostsQuery,allPostsQuery } from './queries';
 
 const collectionRef = collection(firestoreDB, 'user-posts-test1');
 
 
 export const getAllPosts = async ({ lastSnapshot }) => {
 
+    let neededQuery = allPostsQuery({ lastSnapshot })
     try {
-        if (!lastSnapshot) {
 
-            const allPostsQuery = query(
-                collectionRef,
-                orderBy("createdAt", "desc"),
-                limit(11)
-            )
+        const querySnapshot = await getDocs(neededQuery)
 
-            const querySnapshot = await getDocs(allPostsQuery)
+        let moreAvailable = false
 
-            let moreAvailable = false
+        let slicedDocs = querySnapshot.docs
 
-            let slicedDocs = querySnapshot.docs
+        if (slicedDocs.length > 10) {
 
-            if (slicedDocs.length > 10) {
+            moreAvailable = true
 
-                moreAvailable = true
-
-                slicedDocs = slicedDocs.slice(0, 10)
-            }
-
-            let lastDoc = slicedDocs[slicedDocs.length - 1]
-
-            let newPosts = slicedDocs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }))
-
-            return { newPosts, lastDoc, moreAvailable }
-
-        } else {
-            let lastDocQuery = query(collectionRef, orderBy('createdAt', 'desc'), startAfter(lastSnapshot), limit(11))
-
-            const querySnapshot = await getDocs(lastDocQuery)
-
-            let moreAvailable = false
-
-            let slicedDocs = querySnapshot.docs
-
-            if (slicedDocs.length > 10) {
-
-                moreAvailable = true
-
-                slicedDocs = slicedDocs.slice(0, 10)
-
-            }
-
-            let lastDoc = slicedDocs[slicedDocs.length - 1]
-
-            let newPosts = slicedDocs.map(doc => {
-
-                const docData = doc.data()
-
-                const docId = doc.id
-
-                return { id: docId, ...docData }
-            })
-
-            return { newPosts, lastDoc, moreAvailable }
-
+            slicedDocs = slicedDocs.slice(0, 10)
         }
+
+        let lastDoc = slicedDocs[slicedDocs.length - 1]
+
+        let newPosts = slicedDocs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }))
+
+        return { newPosts, lastDoc, moreAvailable }
+
+
 
     } catch (error) {
         console.log(error);
