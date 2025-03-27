c74bcdba468bb5f64c49a9c54f6e132c27095e77
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-27 21:05:13 +0200

    styling edit to small post template

--- client-app/src/components/smallPostTemplate/SmallPostTemplate.jsx
+++ client-app/src/components/smallPostTemplate/SmallPostTemplate.jsx
@@ -35,7 +35,7 @@ function SmallPostTemplate({ id, title, body, postId, createdAt, category }) {
             </div> */}
 
 
-            <div className="max-w-4xl px-10 my-4 py-6 bg-white rounded-lg shadow-md">
+            <div className="max-w-4xl px-10 my-4 py-6 bg-white rounded-lg shadow-md mx-auto">
                 <div className="flex justify-between items-center">
                     <span className="font-light text-gray-600">{formatedDate(createdAt)}</span>
                     <a className="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500" >{category}</a>
