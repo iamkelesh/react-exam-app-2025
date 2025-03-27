56f72ba165b07a5425ea0ed0393d91687c3afc93
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-27 18:00:03 +0200

    updated home component to get only latest posts

--- client-app/src/components/home/Home.jsx
+++ client-app/src/components/home/Home.jsx
@@ -1,54 +1,25 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
 
-import { Link } from "react-router-dom";
+import { getLatestHomePost } from "../../services/postFirestoreService";
+
 import SmallPostTemplate from "../smallPostTemplate/SmallPostTemplate";
 
-import { getMainPostsPerPage } from "../../services/postFirestoreService";
 
-import Pagination from "../pagination/Pagination";
 
 function Home() {
 
     const [posts, setPosts] = useState([]);
-    const [moreAvailable, SetMoreAvailable] = useState(false);
-    const [currentPage, setCurrentPage] = useState(0);
-
-    const { pageNumber } = useParams()
-    const defaultUrl = "home"
 
     useEffect(() => {
 
-        let tempPageNumber = pageNumber
+        getLatestHomePost()
+            .then((latestPosts) => {
 
-        if (typeof pageNumber === 'undefined') {
-            tempPageNumber = 0
-        } else tempPageNumber = Number(pageNumber)
-
-        if (isNaN(tempPageNumber)) {
-            window.alert('Error with URL. Please try again.')
-            return
-        }
-
-
-        getMainPostsPerPage(Number(pageNumber))
-            .then(({ latestPostResult, morePostsAvailableResult }) => {
-
-                if (latestPostResult.length > 5) {
-                    SetMoreAvailable(true)
-                } else {
-                    SetMoreAvailable(false)
-                }
-
-                setCurrentPage(Number(tempPageNumber))
-
-                SetMoreAvailable(morePostsAvailableResult)
-
-                setPosts(latestPostResult)
+                setPosts(latestPosts)
 
             }).catch(err => console.error(err))
 
-    }, [pageNumber, currentPage])
+    }, [])
 
     return (
         <section className="text-gray-600 body-font overflow-hidden">
@@ -58,14 +29,6 @@ function Home() {
 
                     {posts.map(data => <SmallPostTemplate key={data.id} {...data} />)}
                     {posts.length === 0 ? <h1>There are no posts!</h1> : ''}
-
-
-                    {moreAvailable === true || currentPage > 0 ?
-                        <Pagination
-                            defaultUrl={defaultUrl}
-                            currentPage={currentPage}
-                            moreAvailable={moreAvailable}
-                        /> : "No more posts available!"}
 
                 </div>
             </div>
