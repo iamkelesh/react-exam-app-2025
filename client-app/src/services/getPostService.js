3e9bfe091c7d8407e7a5b8949377b9c81c40cf8a
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-28 22:45:59 +0200

    refactored getPost services

--- client-app/src/services/getPostService.js
+++ client-app/src/services/getPostService.js
@@ -1,49 +1,13 @@
-import { collection, addDoc, getDocs, serverTimestamp, doc, getDoc, orderBy, query, updateDoc, deleteDoc, limit, where, startAfter } from 'firebase/firestore';
+import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
 import { firestoreDB } from '../firebase/config';
-import { homePageQuery, mainPagePostsQuery, myPostsQuery, allPostsQuery, postsByCategoryQuery } from './queries';
+import { homePageQuery, myPostsQuery, postsQuery } from './queries';
 
-const collectionRef = collection(firestoreDB, 'user-posts-test1');
-
-
-export const getAllPosts = async ({ lastSnapshot }) => {
-
-    let neededQuery = allPostsQuery({ lastSnapshot })
-    try {
-
-        const querySnapshot = await getDocs(neededQuery)
-
-        let moreAvailable = false
-
-        let slicedDocs = querySnapshot.docs
-
-        if (slicedDocs.length > 10) {
-
-            moreAvailable = true
-
-            slicedDocs = slicedDocs.slice(0, 10)
-        }
-
-        let lastDoc = slicedDocs[slicedDocs.length - 1]
-
-        let newPosts = slicedDocs.map(doc => ({
-            id: doc.id,
-            ...doc.data()
-        }))
-
-        return { newPosts, lastDoc, moreAvailable }
+const collectionRef = collection(firestoreDB, 'user-posts');
 
 
+export const getAllPost2 = async ({ lastSnapshot, category }) => {
 
-    } catch (error) {
-        console.log(error);
-        // return { newPosts: [], lastDoc: null, moreAvailable: false }
-    }
-
-}
-
-export const getByCategory = async ({ lastSnapshot, category }) => {
-
-    let neededQuery = postsByCategoryQuery({ lastSnapshot, category })
+    let neededQuery = postsQuery({ lastSnapshot, category })
 
     try {
         const querySnapshot = await getDocs(neededQuery)
@@ -87,16 +51,52 @@ export const getLatestHomePost = async () => {
             return { id: docId, ...docData }
         })
 
+        console.log(latestPostResult)
+
         return latestPostResult
     } catch (error) {
+        console.log('getLatestHomePost failed')
         console.log(error)
         return []
     }
 }
 
+export const getMyPostsPerPage = async (userId, pageNumber) => {
+    try {
+
+        const neededQuery = await myPostsQuery(userId, pageNumber)
+
+        const querySnapshot = await getDocs(neededQuery)
+
+        let latestMyPostResult = querySnapshot.docs.map(doc => {
+
+            const docData = doc.data()
+
+            const docId = doc.id
+
+            return { id: docId, ...docData }
+        })
+
+        let moreMyPostsAvailableResult = false
+
+        if (latestMyPostResult.length > 5) {
+
+            moreMyPostsAvailableResult = true
+
+            latestMyPostResult = latestMyPostResult.slice(0, 5)
+        }
+
+        return { moreMyPostsAvailableResult, latestMyPostResult }
+
+
+    } catch (error) {
+        console.error("Error while getting posts at service: ", error)
+    }
+}
+
 export const getPostsDetails = async (postId) => {
 
-    const docRef = doc(firestoreDB, 'user-posts-test1', postId)
+    const docRef = doc(firestoreDB, 'user-posts', postId)
 
     const docSnap = await getDoc(docRef)
 
