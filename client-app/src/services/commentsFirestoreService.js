971c95a0ca3d9e63eccca23611d78bbef1b4a8c4
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-30 18:21:07 +0300

    created new service for comment getting

--- client-app/src/services/commentsFirestoreService.js
+++ client-app/src/services/commentsFirestoreService.js
@@ -1,16 +1,17 @@
 import { collection, addDoc, getDocs, serverTimestamp, doc, getDoc, updateDoc, deleteDoc, query, orderBy, limit, DocumentSnapshot, startAfter } from 'firebase/firestore';
 import { firestoreDB } from '../firebase/config';
+import { getCommentsQuery } from './queries';
 
 function getCommentsCollectionRef(postId) {
     return collection(firestoreDB, `user-posts/${postId}/comments`)
 }
 
-export const addComment = async ({  values, addNewToState, clearState }) => {
+export const addComment = async ({ values, addNewToState, clearState }) => {
 
     const commentsCollectionRef = getCommentsCollectionRef(values.postId)
 
     console.log(values)
-    if(!values.postId) {
+    if (!values.postId) {
         window.alert("Post ID is missing")
         return
     }
@@ -44,7 +45,7 @@ export const getlatestsComments = async ({ postId }) => {
 
     const commentsCollectionRef = getCommentsCollectionRef(postId)
 
-    const commentsQuery = query(commentsCollectionRef, orderBy('createdAt', 'desc'), limit(11))
+    const commentsQuery = query(commentsCollectionRef, orderBy('createdAt', 'desc'), limit(6))
 
     const documentSnapshot = await getDocs(commentsQuery)
 
@@ -52,10 +53,10 @@ export const getlatestsComments = async ({ postId }) => {
         return { id: doc.id, ...doc.data() }
     })
 
-    let moreAvailable = comments.length > 10
+    let moreAvailable = comments.length > 5
 
     if (moreAvailable) {
-        comments = comments.slice(0, 10)
+        comments = comments.slice(0, 5)
     }
 
     return { comments, moreAvailable }
@@ -78,7 +79,7 @@ export const getMoreComments = async ({ postId, lastCommentId }) => {
 
     const lastSnapshot = await getDoc(doc(firestoreDB, `user-posts/${postId}/comments/${lastCommentId}`))
 
-    const newCommentsQuery = query(commentsCollectionRef, orderBy('createdAt', 'desc'), startAfter(lastSnapshot), limit(11))
+    const newCommentsQuery = query(commentsCollectionRef, orderBy('createdAt', 'desc'), startAfter(lastSnapshot), limit(6))
 
     const newCommentsSnapshot = await getDocs(newCommentsQuery)
 
@@ -86,12 +87,50 @@ export const getMoreComments = async ({ postId, lastCommentId }) => {
         return { id: doc.id, ...doc.data() }
     })
 
-    let moreAvailable = newComments.length > 10
+    let moreAvailable = newComments.length > 5
 
     if (moreAvailable) {
-        newComments = newComments.slice(0, 10)
+        newComments = newComments.slice(0, 5)
     }
 
     return { newComments, moreAvailable }
 
+}
+
+export const getComments = async ({ postId, lastSnapshot }) => {
+
+    try {
+        const neededQuery = getCommentsQuery({ postId, lastSnapshot })
+
+        const commentsSnaphot = await getDocs(neededQuery)
+
+        let slicedComments = commentsSnaphot.docs
+
+        let moreAvailable = slicedComments.length > 5
+
+        if (moreAvailable) {
+            
+            slicedComments = slicedComments.slice(0, 5)
+        }
+
+        let lastComment = commentsSnaphot.docs[commentsSnaphot.docs.length - 1]
+
+        let newComments = slicedComments.map(doc => {
+            return { id: doc.id, ...doc.data() }
+        })
+
+
+        return {
+            newComments,
+            lastComment,
+            moreAvailable
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            newComments: [],
+            lastComment: null,
+            moreAvailable: false
+        }
+    }
 }
