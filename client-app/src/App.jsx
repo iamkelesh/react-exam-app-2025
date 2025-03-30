03eff6eb775e3db2298e696db81283155673f5e0
Deyan Kamburov <dbkamburov00@gmail.com>
2025-03-31 01:05:55 +0300

    Added error context provider to app

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -17,6 +17,7 @@ import SearchResultsComponent from "./components/searchResults/SearchResultsComp
 import MyPosts from "./components/myPosts/MyPosts"
 import ErrorBanner from "./components/errorBanner/ErrorBanner"
 import Saved from "./components/saved/Saved"
+import { ErrorProvider } from "./contexts/errorContext"
 
 
 function App() {
@@ -26,96 +27,99 @@ function App() {
     <>
 
       <AuthProvider>
-
-        <div className="app-container mx-auto">
-
-          <Header />
-
-          <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
-            <div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-            ></div>
-          </div>
+        <ErrorProvider>
 
 
-          <main className="main-content">
-            <div className="main-wrapper relative">
+          <div className="app-container mx-auto">
 
-              <ErrorBanner />
+            <Header />
 
-
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/home' element={<Home />} />
-                <Route path='/posts/all-posts' element={<AllPosts />} />
-
-                <Route path='/search/:searchInput' element={<SearchResultsComponent />} />
-
-                <Route path='/user/:profileId' element={< Profile />} />
-
-
-                <Route path='/user/my-posts' element={
-                  <AuthGuard>
-                    <MyPosts />
-                  </AuthGuard>} />
-
-                <Route path='/user/saved-posts' element={
-                  <AuthGuard>
-                    <Saved />
-                  </AuthGuard>} />
-
-                <Route path='/posts/details/:postId' element={<PostDetails />} />
-
-                <Route path='/posts/create' element={
-                  <AuthGuard>
-                    <Create />
-                  </AuthGuard>
-                } />
-
-                <Route path='/posts/edit/:postId' element={
-                  <AuthGuard>
-                    <UpdatePost />
-                  </AuthGuard>
-                } />
-
-
-                <Route path="/user/login" element={<Login />} />
-                <Route path='/user/register' element={<Register />} />
-
-                <Route path='/user/logout' element={
-                  <AuthGuard>
-                    <Logout />
-                  </AuthGuard>
-                } />
-
-                <Route path='/my-profile' element={
-                  <AuthGuard>
-                    <Profile />
-                  </AuthGuard>
-                } />
-
-
-                <Route
-                  path="*"
-                  element={
-                    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
-                      <h1 className="text-4xl font-bold text-red-600 mb-4">404 - Page Not Found</h1>
-                      <p className="text-lg text-gray-700 mb-6">
-                        Oops! The page you are looking for does not exist or an error occurred.
-                      </p>
-                      <Link
-                        to="/"
-                        className="px-6 py-3 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition duration-300"
-                      >
-                        Go Back to Home
-                      </Link>
-                    </div>
-                  }
-                />
-              </Routes>
+            <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
+              <div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+              ></div>
             </div>
-          </main>
 
-        </div>
+
+            <main className="main-content">
+              <div className="main-wrapper relative">
+
+                <ErrorBanner />
+
+
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/home' element={<Home />} />
+                  <Route path='/posts/all-posts' element={<AllPosts />} />
+
+                  <Route path='/search/:searchInput' element={<SearchResultsComponent />} />
+
+                  <Route path='/user/:profileId' element={< Profile />} />
+
+
+                  <Route path='/user/my-posts' element={
+                    <AuthGuard>
+                      <MyPosts />
+                    </AuthGuard>} />
+
+                  <Route path='/user/saved-posts' element={
+                    <AuthGuard>
+                      <Saved />
+                    </AuthGuard>} />
+
+                  <Route path='/posts/details/:postId' element={<PostDetails />} />
+
+                  <Route path='/posts/create' element={
+                    <AuthGuard>
+                      <Create />
+                    </AuthGuard>
+                  } />
+
+                  <Route path='/posts/edit/:postId' element={
+                    <AuthGuard>
+                      <UpdatePost />
+                    </AuthGuard>
+                  } />
+
+
+                  <Route path="/user/login" element={<Login />} />
+                  <Route path='/user/register' element={<Register />} />
+
+                  <Route path='/user/logout' element={
+                    <AuthGuard>
+                      <Logout />
+                    </AuthGuard>
+                  } />
+
+                  <Route path='/my-profile' element={
+                    <AuthGuard>
+                      <Profile />
+                    </AuthGuard>
+                  } />
+
+
+                  <Route
+                    path="*"
+                    element={
+                      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
+                        <h1 className="text-4xl font-bold text-red-600 mb-4">404 - Page Not Found</h1>
+                        <p className="text-lg text-gray-700 mb-6">
+                          Oops! The page you are looking for does not exist or an error occurred.
+                        </p>
+                        <Link
+                          to="/"
+                          className="px-6 py-3 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition duration-300"
+                        >
+                          Go Back to Home
+                        </Link>
+                      </div>
+                    }
+                  />
+                </Routes>
+              </div>
+            </main>
+
+          </div>
+        </ErrorProvider>
       </AuthProvider>
 
     </>
--- client-app/src/components/errorBanner/ErrorBanner.jsx
+++ client-app/src/components/errorBanner/ErrorBanner.jsx
@@ -1,30 +1,15 @@
-import { useState } from "react"
+import { useContext } from "react"
 
+import ErrorContext from "../../contexts/errorContext"
 
 export default function ErrorBanner() {
 
-    const [show, setShow] = useState(false)
-    const [errorMessage, setErrorMessage] = useState('')
 
-    function showError(message) {
-        setErrorMessage(message)
-        setShow(true)
-
-        setTimeout(() => {
-            setShow(false)
-            setErrorMessage('')
-        }, 3000)
-    }
-
-    function hideError() {
-        setShow(false)
-        setErrorMessage('')
-    }
-
+    const { errorState, hideErrorHandler } = useContext(ErrorContext)
 
     return (
         <div
-            className={`fixed top-0 left-0 w-full z-50transition-transform duration-300 ${show ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
+            className={`fixed top-0 left-0 w-full z-50transition-transform duration-300 ${errorState.showError ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
                 }`}
         >
             <div role="alert" className="relative w-full text-base font-regular px-4 py-4 rounded-lg bg-red-500 text-white flex shadow-lg">
@@ -32,12 +17,12 @@ export default function ErrorBanner() {
                     <p className="font-bold text-black">
                         ☹️ There was an error
                         <span className="text-white">
-                            - {errorMessage}
+                            - {errorState.errorMessage}
                         </span>
                     </p>
                 </div>
                 <button
-                    onClick={hideError}
+                    onClick={hideErrorHandler}
                     className=" align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
                     <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
