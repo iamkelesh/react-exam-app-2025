88c37f8f24eeeff53e6d0d40fd81718153d9d551
Deyan Kamburov <dbkamburov00@gmail.com>
2025-04-01 05:13:59 +0300

    add vite-bundle-analyzer to enhance bundle analysis in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { analyzer } from 'vite-bundle-analyzer'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    analyzer(),
+  ],
 })
