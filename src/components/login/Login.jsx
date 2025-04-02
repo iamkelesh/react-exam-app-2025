4b482850815d90e0f393dcc321f4f21149717b29
Deyan Kamburov <dbkamburov00@gmail.com>
2025-04-02 23:29:30 +0300

    add pending request handling in Login component

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { Link } from "react-router-dom";
 
 import AuthContext from '../../contexts/authContext';
@@ -11,7 +11,27 @@ const initialValues = {
 
 function Login() {
     const { newLoginHandler } = useContext(AuthContext)
-    const { values, onChange, onSubmit } = useForm({ submitHandler: newLoginHandler, initialValues })
+
+    const { pending } = useRef(false)
+
+    const formHandler = async (e) => {
+        if (pending.current) {
+            return
+        }
+
+        pending.current = true
+
+        try {
+            await newLoginHandler(e)
+            pending.current = false
+
+        } catch (error) {
+            console.log(error);
+            pending.current = false
+        }
+    }
+
+    const { values, onChange, onSubmit } = useForm({ submitHandler: formHandler, initialValues })
 
     return (
         <div className="pt-48 flex items-center justify-center p-4">
@@ -24,7 +44,7 @@ function Login() {
                         <input
                             type="email"
                             name='email'
-                            
+
                             onChange={onChange}
                             value={values.email}
                             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
@@ -39,7 +59,7 @@ function Login() {
                             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
                             placeholder="••••••••"
                             name='password'
-                            
+
                             onChange={onChange}
                             value={values.password}
                         />
